fa388ecc002582e649eef99e4e2aea1df3c53bc8
devonhickle <devonhickle@yahoo.com>
2023-09-04 10:39:11 -0500

    still working on navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav clas="navbar navbar-expand-lg navbar-dark bg-dark">
         <!-- Replace DevDevPortal with a logo eventually -->
         <a class="navbar-brand" href="#">DevDevPortal</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" />
         </button>
         <div class="collapse navbar-collapse" id="navbarContent">
@@ -10,7 +10,28 @@
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
             </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
     </nav>
 </template>
